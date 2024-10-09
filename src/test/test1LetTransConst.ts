5da01744147747ccc92d245c01011564812a6dcb
alamhubb <alamhubb@163.com>
2024-10-10 01:28:30 +0800

    新建测试

--- src/test/test1LetTransConst.ts
+++ src/test/test1LetTransConst.ts
@@ -1,23 +1,27 @@
 import AlienLexer from "../alien/AlienLexer";
-import { es6Tokens } from "../es6/Es6Tokens";
-import { AlienMappingParser } from "../mappingParser/AlienMappingParser";
-import TypescriptGenerator, {gen} from "../ts/TypescriptGenerator";
+import {es6Tokens} from "../es6/Es6Tokens";
+import {AlienMappingParser} from "../mappingParser/AlienMappingParser";
+import {gen} from "../ts/TypescriptGenerator";
 
 
-export const code1 = `
-let a = 1
-`;
+const code = `let a = 1`;
 const lexer = new AlienLexer(es6Tokens);
-const tokens = lexer.lexer(code1);
+const tokens = lexer.lexer(code);
 const parser = new AlienMappingParser(tokens);
 
 const res = parser.program();
 
 
-
 console.log(res)
 // console.log(res)
 // console.log(parser.curCst)
-const code = gen.generator(res);
+const resCode = gen.generator(res);
+
+const expect = 'const a = 1'
+
 // console.log(res)
-console.log(code)
+console.log(`
+result: ${resCode === expect ? '\x1b[32msuccess' : '\x1b[31merror'}\x1b[0m
+input: ${code}
+output: ${resCode}
+`)

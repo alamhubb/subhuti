0984c3a55e204d37be78b0acc57f7660f367a4f1
alamhubb <alamhubb@163.com>
2024-10-10 18:11:57 +0800

    支持成功校验

--- test/TestUtil.ts
+++ test/TestUtil.ts
@@ -3,11 +3,16 @@ export class TestObj {
     test: Function;
     expect: string;
 }
+
 export default class TestUtil {
     static test(testObj: TestObj) {
         const input = testObj.input;
-        const output = testObj.test(input);
-        const expect = testObj.expect;
+        let output = testObj.test(input);
+        let expect = testObj.expect;
+        // output = `{"name":"program","children":[{"name":"letKeywords","children":[{"name":"let","children":[],"tokens":[],"value":"let"}],"tokens":[{"tokenName":"let","tokenValue":"let"}]},{"name":"identifierEqual","children":[{"name":"identifier","children":[],"tokens":[],"value":"a"},{"name":"equal","children":[],"tokens":[],"value":"="}],"tokens":[{"tokenName":"identifier","tokenValue":"a"},{"tokenName":"equal","tokenValue":"="}]},{"name":"assignmentExpression","children":[{"name":"integer","children":[],"tokens":[],"value":"1"}],"tokens":[{"tokenName":"integer","tokenValue":"1"}]}],"tokens":[]}`
+        // expect = `{"name":"program","children":[{"name":"letKeywords","children":[{"name":"let","children":[],"tokens":[],"value":"let"}],"tokens":[{"tokenName":"let","tokenValue":"let"}]},{"name":"identifierEqual","children":[{"name":"identifier","children":[],"tokens":[],"value":"a"},{"name":"equal","children":[],"tokens":[],"value":"="}],"tokens":[{"tokenName":"identifier","tokenValue":"a"},{"tokenName":"equal","tokenValue":"="}]},{"name":"assignmentExpression","children":[{"name":"integer","children":[],"tokens":[],"value":"1"}],"tokens":[{"tokenName":"integer","tokenValue":"1"}]}],"tokens":[]}`
+        // console.log(output)
+        // console.log(expect)
 
         console.log(`
 result: ${output === expect ? '\x1b[32msuccess' : '\x1b[31merror'}\x1b[0m
--- test/test1LetJson.ts
+++ test/test1LetJson.ts
@@ -5,16 +5,18 @@ import { MappingCst, typescriptGenerator } from "../src/ts/TypescriptGenerator";
 import TestUtil from "./TestUtil";
 import Es6Parser from "../src/es6/Es6Parser";
 import JsonUtil from "../src/utils/JsonUtil";
+
+const res = `{"name":"program","children":[{"name":"letKeywords","children":[{"name":"let","children":[],"tokens":[],"value":"let"}],"tokens":[{"tokenName":"let","tokenValue":"let"}]},{"name":"identifierEqual","children":[{"name":"identifier","children":[],"tokens":[],"value":"a"},{"name":"equal","children":[],"tokens":[],"value":"="}],"tokens":[{"tokenName":"identifier","tokenValue":"a"},{"tokenName":"equal","tokenValue":"="}]},{"name":"assignmentExpression","children":[{"name":"integer","children":[],"tokens":[],"value":"1"}],"tokens":[{"tokenName":"integer","tokenValue":"1"}]}],"tokens":[]}`
+
 TestUtil.test({
     input: `let a = 1`,
-    expect: 'const a = 1',
+    expect: res,
     test(input: string) {
         const lexer = new AlienLexer(es6Tokens);
         const tokens = lexer.lexer(input);
         const parser = new Es6Parser(tokens);
         const res = parser.program();
-        console.log(66666)
-        console.log(JsonUtil.toJson(res))
+        return JsonUtil.toJson(res)
         // alienMappingParser.openMappingMode()
         // return typescriptGenerator.generator(res);
     },

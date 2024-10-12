e11b156cffc468d2bd51f93aea7cf984c6199934
alamhubb <alamhubb@163.com>
2024-10-13 01:58:07 +0800

    成功执行完毕所有方法

--- src/subhuti/SubhutiParser.ts
+++ src/subhuti/SubhutiParser.ts
@@ -1,16 +1,19 @@
 import SubhutiMatchToken from "./struct/SubhutiMatchToken";
 import SubhutiCst from "./struct/SubhutiCst";
 import JsonUtil from "../utils/JsonUtil";
-import { SubhutiCreateToken } from "./struct/SubhutiCreateToken";
+import {SubhutiCreateToken} from "./struct/SubhutiCreateToken";
+
 export class SubhutiParserOr {
     alt: Function;
 }
+
 enum LogicType {
     consume = 'consume',
     or = 'or',
     many = 'many',
     option = 'option'
 }
+
 export function SubhutiRule(targetFun: any, context) {
     const ruleName = targetFun.name;
     return function () {
@@ -18,11 +21,13 @@ export function SubhutiRule(targetFun: any, context) {
         return this.generateCst(this.curCst);
     };
 }
+
 function CheckMethodCanExec(newTargetFun: any) {
     return function (...args: any[]) {
         return this.checkMethodCanExec(newTargetFun, args);
     };
 }
+
 export default class SubhutiParser {
     _tokens: SubhutiMatchToken[];
     initFlag = true;
@@ -30,39 +35,50 @@ export default class SubhutiParser {
     cstStack: SubhutiCst[] = [];
     _continueExec = true;
     thisClassName: string;
+
     get continueExec() {
         return this._continueExec;
     }
+
     setContinueExec(flag: boolean) {
         this._continueExec = flag;
     }
+
     setCurCst(curCst: SubhutiCst) {
         this.curCst = curCst;
     }
+
     getCurCst() {
         return this.curCst;
     }
+
     get tokens() {
         this.checkContinueExec();
         return this._tokens;
     }
+
     setTokens(tokens?: SubhutiMatchToken[]) {
         this._tokens = tokens;
         this.checkTokens();
     }
+
     constructor(tokens?: SubhutiMatchToken[]) {
         if (tokens) {
             this.setTokens(tokens);
         }
         this.thisClassName = this.constructor.name;
     }
+
     get allowError() {
         return this._allowError;
     }
+
     _allowError = false;
+
     setAllowError(allowError: boolean) {
         this._allowError = allowError;
     }
+
     checkMethodCanExec(newTargetFun: any, args: any[]) {
         if (!this.continueExec) {
             if (this.allowError) {
@@ -78,6 +94,7 @@ export default class SubhutiParser {
         }
         return newTargetFun.apply(this, args);
     }
+
     //首次执行，则初始化语法栈，执行语法，将语法入栈，执行语法，语法执行完毕，语法出栈，加入父语法子节点
     @CheckMethodCanExec
     subhutiRule(targetFun: any, ruleName: string) {
@@ -93,8 +110,7 @@ export default class SubhutiParser {
         if (initFlag) {
             //执行完毕，改为true
             this.initFlag = true;
-        }
-        else {
+        } else {
             if (cst) {
                 const parentCst = this.cstStack[this.cstStack.length - 1];
                 parentCst.children.push(cst);
@@ -102,6 +118,7 @@ export default class SubhutiParser {
             }
         }
     }
+
     //执行语法，将语法入栈，执行语法，语法执行完毕，语法出栈
     processCst(ruleName: string, targetFun: Function) {
         let cst = new SubhutiCst();
@@ -119,10 +136,37 @@ export default class SubhutiParser {
         }
         return null;
     }
+
     @CheckMethodCanExec
     //匹配1次或者N次
-    AT_LEAST_ONE(tokenName: any) {
+    AT_LEAST_ONE(fun: Function) {
+        this.checkContinueExec();
+        let index = 0
+        while (this.continueExec) {
+            if (index > 0) {
+                this.setAllowError(true);
+                this.allowErrorStack.push(true);
+                const tokensBackup = JsonUtil.cloneDeep(this.tokens);
+                if (!this.tokens.length) {
+                    break;
+                }
+                fun();
+                //If the match fails, the tokens are reset.
+                if (!this.continueExec) {
+                    this.setTokens(tokensBackup);
+                }
+                //因为允许空
+                this.allowErrorStack.pop();
+                this.setContinueExec(true);
+                this.setAllowError(!!this.allowErrorStack.length);
+            } else {
+                fun();
+            }
+            index++
+        }
+        return this.getCurCst();
     }
+
     @CheckMethodCanExec
     //匹配0次或者1次
     OPTION(fun: Function) {
@@ -141,10 +185,12 @@ export default class SubhutiParser {
         this.setAllowError(!!this.allowErrorStack.length);
         return this.getCurCst();
     }
+
     @CheckMethodCanExec
     consume(tokenName: SubhutiCreateToken) {
         return this.consumeToken(tokenName.name);
     }
+
     //消耗token，将token加入父语法
     consumeToken(tokenName: string) {
         let popToken = this.tokens[0];
@@ -166,8 +212,10 @@ export default class SubhutiParser {
         this.curCst.tokens.push(popToken);
         return this.generateCst(cst);
     }
+
     allowErrorStack = [];
     ruleExecErrorStack = [];
+
     @CheckMethodCanExec
     //or语法，遍历匹配语法，语法匹配成功，则跳出匹配，执行下一规则
     or(subhutiParserOrs: SubhutiParserOr[]) {
@@ -195,6 +243,7 @@ export default class SubhutiParser {
         this.setAllowError(!!this.allowErrorStack.length);
         return this.getCurCst();
     }
+
     @CheckMethodCanExec
     //匹配0次或者N次
     MANY(fun: Function) {
@@ -218,9 +267,11 @@ export default class SubhutiParser {
         this.setAllowError(!!this.allowErrorStack.length);
         return this.getCurCst();
     }
+
     generateCst(cst: SubhutiCst) {
         return cst;
     }
+
     //随便调用，就是重复校验
     checkContinueExec() {
         //continueExec should be true, because CheckMethodCanExec makes a judgment
@@ -228,6 +279,7 @@ export default class SubhutiParser {
             throw new Error('syntax error');
         }
     }
+
     checkTokens() {
         if (!this._tokens?.length) {
             if (!this.allowError) {

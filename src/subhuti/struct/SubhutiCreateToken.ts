4b0bf83ba68f145e705c8c1082e28ff451e7f03d
alamhubb <alamhubb@163.com>
2024-10-18 00:33:16 +0800

    支持ovs语法

--- src/subhuti/struct/SubhutiCreateToken.ts
+++ src/subhuti/struct/SubhutiCreateToken.ts
@@ -1,24 +1,37 @@
+import {Es5TokensName} from "../../syntax/es5/Es5Tokens";
+
 export enum SubhutiCreateTokenGroupType {
     skip = 'skip'
 }
+
 export class SubhutiCreateToken {
     name: string;
     pattern?: RegExp;
     isKeyword?: boolean;
     group?: string;
+    value?: string;
     categories?: any;
+
     constructor(osvToken: SubhutiCreateToken) {
         this.name = osvToken.name;
         this.pattern = osvToken.pattern;
+        this.value = osvToken.pattern.source;
         this.isKeyword = false;
         this.group = osvToken.group;
     }
 }
+
 export function createToken(osvToken: SubhutiCreateToken) {
     return new SubhutiCreateToken(osvToken);
 }
-export function createKeywordToken(osvToken: SubhutiCreateToken) {
-    const token = new SubhutiCreateToken(osvToken);
+
+export function createKeywordToken(name: string, value: string) {
+    const token = new SubhutiCreateToken({name: name, pattern: new RegExp(value)});
     token.isKeyword = true;
+    return token;
+}
+
+export function createStringToken(name: string, value: string) {
+    const token = new SubhutiCreateToken({name: name, pattern: new RegExp(value)});
     return token;
 }

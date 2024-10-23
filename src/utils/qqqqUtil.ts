2fd3b7f34140ae4e9a509b8ed5aa864c1ba876cf
alamhubb <alamhubb@163.com>
2024-10-23 22:20:45 +0800

    逻辑正确了

--- src/utils/qqqqUtil.ts
+++ src/utils/qqqqUtil.ts
@@ -1,6 +1,10 @@
 export default class QqqqUtil {
     static log(str: any) {
-        console.log(String(str))
+        const error = new Error(str)
+        const stack = error.stack.split('\n');  // 将堆栈按行分割
+        const trace = stack.slice(2, 4);  // 获取第二行的调用信息（实际函数的调用位置）
+        console.log(stack[0].trim());  // 打印一行的调用信息
+        console.log(trace);  // 打印一行的调用信息
     }
 
     static test(str: any) {

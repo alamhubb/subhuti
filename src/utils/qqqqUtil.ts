fb4b5c652c47730421f18b5059bae7c17fdc343b
alamhubb <alamhubb@163.com>
2024-10-24 02:49:26 +0800

    逻辑正确了

--- src/utils/qqqqUtil.ts
+++ src/utils/qqqqUtil.ts
@@ -2,7 +2,7 @@ export default class QqqqUtil {
     static log(str: any) {
         const error = new Error(str)
         const stack = error.stack.split('\n');  // 将堆栈按行分割
-        const trace = stack.slice(2, 4);  // 获取第二行的调用信息（实际函数的调用位置）
+        const trace = stack.slice(2, 6);  // 获取第二行的调用信息（实际函数的调用位置）
         console.log(stack[0].trim());  // 打印一行的调用信息
         console.log(trace.join('\n'));  // 打印一行的调用信息
     }

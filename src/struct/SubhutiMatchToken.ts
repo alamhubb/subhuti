4db2f03abe79c50a1906ed6e2e730145266fe409
qinkaiyuan <qinkaiyuan94@163.com>
2024-11-03 15:04:45 +0800

    支持高亮关键词

--- src/struct/SubhutiMatchToken.ts
+++ src/struct/SubhutiMatchToken.ts
@@ -2,9 +2,16 @@ export default class SubhutiMatchToken {
     tokenName: string;
     //只能为字符串，为parser解析时输入的字符串
     tokenValue: string;
+    rowNum?: number;
+    columnStartNum?: number;
+    columnEndNum?: number;
+
     constructor(osvToken: SubhutiMatchToken) {
         this.tokenName = osvToken.tokenName;
         this.tokenValue = osvToken.tokenValue;
+        this.rowNum = osvToken.rowNum;
+        this.columnStartNum = osvToken.columnStartNum;
+        this.columnEndNum = osvToken.columnEndNum;
     }
 }
 export function createMatchToken(osvToken: SubhutiMatchToken) {

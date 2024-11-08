fd937ba3e02163937ba79d5f69eddcb5cc15bad4
alamhubb <alamhubb@163.com>
2024-11-08 10:50:35 +0800

    ast改为subhutiast

--- src/syntax/es5/TestStringReg.ts
+++
@@ -1,6 +0,0 @@
-const pattern = /(["'])((?:\\\1|(?:(?!\1|\n|\r).)*)*)\1/;
-const newPattern = new RegExp('^(?:' + pattern.source + ')');
-const input = '"vue"'
-// token正则匹配
-const matchRes = input.match(pattern); // 尝试匹配输入字符串
-console.log(matchRes)

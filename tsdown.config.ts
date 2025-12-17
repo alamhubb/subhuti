0608557a3cca43443eab45f469ee670d6d37aff6
alamhubb <alamhubb@163.com>
2025-12-17 11:13:09 +0800

    cssts问题修复

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'tsdown'
+import {defineConfig} from 'tsdown'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: 'esm',
-  dts: true,
-  clean: true,
-  outDir: 'dist',
-  // 编译装饰器
-  target: 'es2020',
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
+    dts: true,
+    clean: true,
+    outDir: 'dist',
+    // 编译装饰器
+    target: 'es2020',
 })
 

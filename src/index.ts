cca6c6f8d990c61be89aac67c77eaeb2576b94fc
alamhubb <@012aihO>
2026-01-03 23:39:12 +0800

    1

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+// Core Parser
+export { default as SubhutiParser, Subhuti, SubhutiRule } from './SubhutiParser.ts'
+export type { RuleFunction, SubhutiParserOr, SubhutiBackData, PartialMatchRecord, ParseRecordNode, SubhutiParserOptions, SubhutiTokenConsumerConstructor } from './SubhutiParser.ts'
+
+// Lexer
+export { default as SubhutiLexer } from './SubhutiLexer.ts'
+export type { TokenCacheEntry, LexerState } from './SubhutiLexer.ts'
+
+// Lexer Mode
+export { DefaultMode, createMode } from './struct/SubhutiCreateToken.ts'
+export type { LexerMode, LexerModeBrand } from './struct/SubhutiCreateToken.ts'
+
+// Token Consumer & Lookahead
+export { default as SubhutiTokenConsumer } from './SubhutiTokenConsumer.ts'
+export { default as SubhutiTokenLookahead } from './SubhutiTokenLookahead.ts'
+
+// CST & Token structures
+export { default as SubhutiCst } from './struct/SubhutiCst.ts'
+export type { SubhutiSourceLocation, SubhutiPosition } from './struct/SubhutiCst.ts'
+export { default as SubhutiMatchToken, createMatchToken } from './struct/SubhutiMatchToken.ts'
+export {
+    SubhutiCreateToken,
+    emptyValue,
+    createToken,
+    createKeywordToken,
+    createRegToken,
+    createValueRegToken,
+    createEmptyValueRegToken
+} from './struct/SubhutiCreateToken.ts'
+export type { SubhutiTokenLookahead as SubhutiTokenLookaheadConfig, SubhutiTokenContextConstraint } from './struct/SubhutiCreateToken.ts'
+
+// Error handling
+export { SubhutiErrorHandler, ParsingError } from './SubhutiError.ts'
+export type { ErrorDetails } from './SubhutiError.ts'
+export type { SubhutiErrorType, LeftRecursionInfo, InfiniteLoopInfo, ErrorPosition, SubhutiError } from './SubhutiErrorTypes.ts'
+
+// Debug utilities
+export { SubhutiDebugUtils, SubhutiTraceDebugger } from './SubhutiDebug.ts'
+export type { RuleStats } from './SubhutiDebug.ts'
+export { setShowRulePath, getShowRulePath, SubhutiDebugRuleTracePrint, TreeFormatHelper } from './SubhutiDebugRuleTracePrint.ts'
+export type { RuleStackItem, OrBranchInfo } from './SubhutiDebugRuleTracePrint.ts'
+
+// Packrat Cache
+export { SubhutiPackratCache } from './SubhutiPackratCache.ts'
+export type { SubhutiPackratCacheResult, SubhutiPackratCacheStatsReport } from './SubhutiPackratCache.ts'
+
+// Validation
+export * from './validation'
+
+// Utilities
+export { LogUtil } from './logutil.ts'
+export { default as JsonUtil } from './JsonUtil.ts'
